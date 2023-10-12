0f71e5c1da1915b1f75389ca626316eb66a525dc
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-10-12 16:35:35 +0530

    reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,3 +1,2 @@
-//reduce use in the outside of the componanent
-
-//and use in the functional componanet
+//reduce use in the outside of the componanent.
+//And use in the functional componanet and also use in the class componanet.
